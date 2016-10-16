410a4ca4671b1ecaab8f6a7d615acd94664d4fe5
acemullet <mlozoyaburciaga@csumb.edu>
2016-10-15 21:22:10 -0700

    testing form to db

--- index.js
+++ index.js
@@ -40,21 +40,20 @@ app.get('/db', function (request, response) {
 });
 
 app.post('/myaction', function(request, response) {
-  var name = request.body.firstname +"";
+  var name = request.body.vList +"";
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     //client.query("INSERT INTO test_table values($1, $2)", [2, name]);
     client.query('INSERT INTO test_table values($1, $2)', [2, name], function(err, result) {
       done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    }); }
+      if (err){ console.error(err); response.send("Error " + err); }
+      else{ client.query('SELECT * FROM test_table', function(err, result) {
+          done();
+          if (err)
+          { console.error(err); response.send("Error " + err); }
+          else
+          { response.render('pages/db', {results: result.rows} ); }
+       });
+      }
     });
   });
 });
