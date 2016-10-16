08a6f36080a3ce5d62a9909acde492e39361b00a
acemullet <mlozoyaburciaga@csumb.edu>
2016-10-15 19:53:46 -0700

    testing form to db

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.get('/db', function (request, response) {
 app.post('/myaction', function(request, response) {
   var name = request.body.firstname +"";
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('INSERT into test_table values (1, name);', function(err, result) {
+    client.query('INSERT into test_table values (1, `name`);', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
