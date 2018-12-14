d522e8a9a5ca2031a9cfccb54c90a594a7e842cd
Miguel <miguel.lb@digitalnest.org>
2018-12-14 14:56:19 -0800

    Testing postList

--- index.js
+++ index.js
@@ -102,6 +102,28 @@ app.post('/postList', function(request, response) {
   });
 });
 
+
+// START - FOR TESTING ON UNITY //
+app.post('/postList', function(request, response) {
+  var name = request.body.vList +"";
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    //client.query("INSERT INTO test_table values($1, $2)", [2, name]);
+    client.query("INSERT INTO test_table values(1, 'unity')", [2, name], function(err, result) {
+      done();
+      if (err){ console.error(err); response.send("Error " + err); }
+      else{ client.query('SELECT * FROM test_table', function(err, result) {
+          done();
+          if (err)
+          { console.error(err); response.send("Error " + err); }
+          else
+          { response.render('pages/db', {results: result.rows} ); }
+       });
+      }
+    });
+  });
+});
+// END - FOR TESTING ON UNITY //
+
 app.get('/popRequest', function(request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT id, xpos, ypos, status FROM canvas_map_dev WHERE status = 0 order by id asc fetch first 1 rows only', 
