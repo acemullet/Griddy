71cd4e31cb2b3c4c63231ad6cd540bd2224e93eb
Miguel <miguel.lb@digitalnest.org>
2018-12-14 16:29:49 -0800

    Testing postList

--- index.js
+++ index.js
@@ -105,11 +105,10 @@ app.get('/db', function (request, response) {
 
 // START - FOR TESTING ON UNITY //
 app.post('/postList', function(request, response) {
-  var req = request.body.vList;
-  var bitmap = request.body.vList +"";
+  var bitmap = request.body.bitmap;
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     //client.query("INSERT INTO test_table values($1, $2)", [2, name]);
-    client.query('UPDATE canvas_map_dev2 SET bitmap=$2 WHERE id=$1', [1, bitmap], function(err, result) {
+    client.query('UPDATE canvas_map_dev2 SET bitmap=$2 WHERE id=$1', [1, bitmap.toString()], function(err, result) {
       done();
       if (err){ console.error(err); response.send("Error " + err); }
       else{ client.query('SELECT * FROM canvas_map_dev2', function(err, result) {
@@ -117,7 +116,7 @@ app.post('/postList', function(request, response) {
           if (err)
           { console.error(err); response.send("Error " + err); }
           else
-          { response.send("Name : " + req); }
+          { response.send("Name : " + bitmap.toString); }
           /*else
           { response.render('pages/db', {results: result.rows} ); }*/
        });
